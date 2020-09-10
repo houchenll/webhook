c24b0492697ae5af151113129e0a4c99e8881181
houchenll <1498096942@qq.com>
2020-09-10 21:18:45 +0800

    只有指定分支才部署

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const projectHandler = (event, action) => {
     const project = event.payload.repository.name // 仓库的名字
     console.log(project);
     const branch = event.payload.ref
-    if (projects.includes(project)) {
+    console.log(branch)
+    // 只有release_v1更新时才部署
+    if (projects.includes(project) && branch.indexOf("release_v1") != -1) {
         console.log(new Date(), `Received a ${action} event for ${project} to ${branch}`)
         shell.exec(`sh ./projects/${project}.sh`, (code, stdout, stderr) => {
             console.log(new Date(), 'Exit code:', code)
             console.log(new Date(), '执行完毕！错误信息：？', stderr)
         })
-
+    } else {
+        console.log('非release_v1，不部署')
     }
 }
 
